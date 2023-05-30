51cf1ee86ca8bad2732bbfa2b9b5c4dc52357919
sundararajan-git <sundararajanselvarasu@gmail.com>
2023-05-30 20:38:47 +0530

    V2

--- WITH_JS/calculater/script.js
+++ WITH_JS/calculater/script.js
@@ -1,63 +1,57 @@
-let btn=document.querySelectorAll('.box');
-let p=document.getElementById('output');
-let ac=document.getElementById('ac');
-let cancel=document.getElementById('cancel');
-let ope=document.getElementById('ope');
-let equal=document.getElementById('equal');
-let t=document.getElementById('top');
+let btn = document.querySelectorAll(".box");
+let p = document.getElementById("output");
+let ac = document.getElementById("ac");
+let cancel = document.getElementById("cancel");
+let ope = document.getElementById("ope");
+let equal = document.getElementById("equal");
+let t = document.getElementById("top");
+
 // print the text bord
-btn.forEach(btn=>
-    btn.addEventListener('click',()=>{
-        if(p.textContent=="Calculate...."){
-        p.textContent="";
-        }
-        p.innerHTML+= btn.textContent;
-    })
-    )
+btn.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (p.textContent == "Calculate....") {
+      p.textContent = "";
+    }
+    p.innerHTML += btn.textContent;
+  })
+);
 
 // all clear button
-ac.addEventListener('click',allClear);
-function allClear(){
-    if(p.textContent!=""){
-        p.innerHTML="Calculate....";
-    } 
-    if(c.textContent!=""){
-        c.innerHTML="";
-    } 
+ac.addEventListener("click", allClear);
+function allClear() {
+  if (p.textContent != "") {
+    p.innerHTML = "Calculate....";
+  }
+  if (c.textContent != "") {
+    c.innerHTML = "";
+  }
 }
 
 // cancel button
-cancel.addEventListener('click',cancelButton);
-function cancelButton(){
-    let del=p.textContent;    
-    let arr=del.split("");
-    if(arr.length==1){
-        p.textContent='Calculate....';
-    }
-    else if(p.textContent=="Calculate...."){
-    }
-    else if(c.textContent!=""){
-        c.innerHTML="";
-    } 
-    else if(p.textContent!=''){
-       arr.pop();
-       p.textContent=arr.join('');
-    }
- 
+cancel.addEventListener("click", cancelButton);
+function cancelButton() {
+  let del = p.textContent;
+  let arr = del.split("");
+  if (arr.length == 1) {
+    p.textContent = "Calculate....";
+  } else if (p.textContent == "Calculate....") {
+  } else if (c.textContent != "") {
+    c.innerHTML = "";
+  } else if (p.textContent != "") {
+    arr.pop();
+    p.textContent = arr.join("");
+  }
 }
 
-let c = document.createElement('p')
+let c = document.createElement("p");
 t.appendChild(c);
-c.innerHTML='';
-equal.addEventListener('click',()=>{
-    c.style.color='orangered';
-    try{
+c.innerHTML = "";
+equal.addEventListener("click", () => {
+  c.style.color = "orangered";
+  try {
     t.appendChild(c);
-    c.innerHTML=eval(p.textContent);
-    }
-    catch{
-        alert("check valid calculation");
-    }
-})
-
-
+    c.innerHTML = eval(p.textContent);
+  } catch {
+    alert("check valid calculation");
+  }
+});
